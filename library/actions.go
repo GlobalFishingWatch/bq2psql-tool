fd824ad99963be06eb71890b8e2e2d3d500f46e9
aperdizs <info@alvaroperdiz.com>
2021-11-15 16:17:04 +0100

    Export funcion import to use it as an external package

---
+++ library/actions.go
@@ -0,0 +1,10 @@
+package library
+
+import (
+	"github.com/GlobalFishingWatch/bq2psql-tool/internal/action"
+	"github.com/GlobalFishingWatch/bq2psql-tool/types"
+)
+
+func ImportBigQueryToPostgres(params types.ImportParams, postgresConfig types.PostgresConfig) {
+	action.ImportBigQueryToPostgres(params, postgresConfig)
+}
